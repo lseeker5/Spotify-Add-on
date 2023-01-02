87f6ba7034984077ed0f2ad23d4d697fcdcc37bd
lseeker5 <118029799+lseeker5@users.noreply.github.com>
2023-01-02 09:45:32 +0800

    done but with status code 400.....

--- client/src/util/Spotify.js
+++ client/src/util/Spotify.js
@@ -1,10 +1,11 @@
+import Axios from "axios"
 let accessToken
 const clientID="97fde84b14fc441f8b1421336027a266"
 const redirectURI = "http://localhost:3000/"
-import Axios from "axios"
 
 
-export default Spotify={
+
+const Spotify={
     getAccessToken(){
         if(accessToken){
             return accessToken
@@ -29,6 +30,7 @@ export default Spotify={
 
     search(para){
         const accessToken=Spotify.getAccessToken()
+        //use accesstoken to search for songs
         return Axios.get(`https://api.spotify.com/v1/search?type=track&q=${para}`,{
             headers: {
                 Authorization: `Bearer ${accessToken}`}
@@ -36,6 +38,7 @@ export default Spotify={
             if(!res.tracks){
                 return []
             }
+            //get the necessary info from all the shit that is given back
             return res.tracks.items.map((track)=>{
                 return({
                     id:track.id,
@@ -50,22 +53,26 @@ export default Spotify={
     },
 
     savePlaylist(name,trackURIs){
-        if(!playlistName||trackURIs.length){
+        if(!name||trackURIs.length){
             return
         }
         const accessToken=Spotify.getAccessToken()
         const headers={Authorization: `Bearer ${accessToken}`}
         let userId
-
+        //get userid
         return Axios.get('https://api.spotify.com/v1/me',{headers: headers}).then(res=>{
             userId=res.id
+            //use userid to create new playlist and get playlist id
             return Axios.post(`/v1/users/${userId}/playlists`,{name:name},{headers:headers})
         }).then(res=>{
             const playlistId=res.id
-            return Axios.post(`/v1/users/${user_id}/playlists/${playlist_id}/tracks`,{uris:trackURIs},{headers:headers})
+            //use userid and playlistid to put tracks into the playlist
+            return Axios.post(`/v1/users/${userId}/playlists/${playlistId}/tracks`,{uris:trackURIs},{headers:headers})
         })
     }
     
 
 
 }
+
+export default Spotify
